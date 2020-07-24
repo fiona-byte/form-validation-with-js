7f5cac3ffef1fd66775475b4874863a812922ca7
unknown <fionaodoboh@gmail.com>
2020-07-24 16:28:21 +0100

    changes

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const form = document.getElementById("form");
-const username = document.getElementById("fullname");
+const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 
+
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault(); //prevents the page from being refreshed
 
     checkInputFields();
 });
-
 
 function checkInputFields() {
     //get the values from the user
@@ -28,6 +28,8 @@ function checkInputFields() {
     if (emailValue === "") {
         //show error message
         setErrorFor(email, "Email cannot be blank");
+    } else if (!validateEmail(emailValue)) {
+        setErrorFor(email, "Please enter a valid email address");
     } else {
         //add success class
         setSuccessFor(email);
@@ -37,7 +39,7 @@ function checkInputFields() {
         //show error message
         setErrorFor(password, "Password cannot be blank");
     } else if (passwordValue.length < 8) {
-        setErrorFor(password, "password should be at least 8 characters");
+        setErrorFor(password, "Password should be at least 8 characters");
     } else {
         //add success class
         setSuccessFor(password);
@@ -46,23 +48,24 @@ function checkInputFields() {
 }
 
 function setErrorFor(input, message) {
-    const formControl = input.parentElement; //form-control div
+    const formControl = input.parentElement; //form-group div
     const errorMessage = formControl.querySelector('small');
 
     //add error message
     errorMessage.innerText = message;
 
     //add error class
-    formControl.className = 'form-control error';
+    formControl.className = 'form-group error';
 }
 
 function setSuccessFor(input) {
-    const formControl = input.parentElement; //form-control div
+    const formControl = input.parentElement; //form-group div
    
     //add success class
-    formControl.className = 'form-control success';
+    formControl.className = 'form-group success';
 }
 
-// function emailCheck (email) {
-
-// }
+function validateEmail(email) {
+    const emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailReg.test(email);
+}
